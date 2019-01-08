23cae8910572295f6f8e79f88075fa039197e592
Fernando Arocho <fernando.arocho@bottlerocketstudios.com>
2019-01-08 11:52:43 -0600

    Adding TVOS target

---
+++ Examples/Scotty-tvOSExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scotty-tvOSExample
+//
+//  Created by Fernando Arocho on 1/8/19.
+//  Copyright © 2019 Bottle Rocket Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
